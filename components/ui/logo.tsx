9f294b5d703fed8bd87351b61dda53bbbc26e5d1
werelddigitaal <163646281+werelddigitaal@users.noreply.github.com>
2024-04-22 21:07:04 +0200

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <div className="w-8 h-8 relative">
-        <Image src="https://werelddigitaal.nl/WereldDigitaal%20Tr-03.png" alt="Logo" layout="fill" objectFit="contain" />
-      </div>
+      <img src="https://werelddigitaal.nl/WereldDigitaal%20Tr-03.png" alt="Logo" className="w-8 h-8" />
     </Link>
   )
 }
