03bf1d80328303e308d1d7428e70268f4d3d823a
werelddigitaal <163646281+werelddigitaal@users.noreply.github.com>
2024-04-22 21:33:02 +0200

    Update newsletter.tsx

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,3 +1,4 @@
+
 export default function Newsletter() {
   return (
     <section>
@@ -37,18 +38,18 @@ export default function Newsletter() {
 
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
-                <h3 className="h3 text-white mb-2">Want more tutorials & guides?</h3>
-                <p className="text-gray-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit nemo expedita voluptas culpa sapiente.</p>
+                <h3 className="h3 text-white mb-2">Ben jij opzoek naar een nieuwe website?</h3>
+                <p className="text-gray-300 text-lg mb-6">Wij helpen je graaf verder.</p>
 
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
-                  <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                    <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
+                  <div className="flex flex-col sm:flex-row justify-start max-w-xs mx-auto sm:max-w-md lg:mx-0">
+                    {/* <input type="text" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Suggestion or idea..." aria-label="Suggestion or idea..." /> */}
+                    <a href="mailto: info@werelddigitaal.nl" className="btn text-white bg-blue-600 hover:bg-blue-700 shadow">Offerte aanvragen</a>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
-                  <p className="text-sm text-gray-400 mt-3">No spam. You can unsubscribe at any time.</p>
+                  <p className="text-sm text-gray-400 mt-3">We will implement your idea or suggestion as soon as we can.</p>
                 </form>
               </div>
 
