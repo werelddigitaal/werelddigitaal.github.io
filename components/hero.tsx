0bf7c6b324d09a3c6d2c1e2cce6b788d88bf6475
werelddigitaal <163646281+werelddigitaal@users.noreply.github.com>
2024-04-22 22:03:57 +0200

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,9 +29,9 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Maak jouw Website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">w</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">o</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">n</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">d</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">e</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">r</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">f</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">u</span><span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">l</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Transformeer je digitale aanwezigheid met WereldDigitaal.nl - waar we niet alleen websites ontwerpen en hosten, maar een wereld van mogelijkheden creëren voor jouw online succes!</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
