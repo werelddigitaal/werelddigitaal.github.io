8ef977f0cf45ffdb6f56e320ba719db600816bdd
werelddigitaal <163646281+werelddigitaal@users.noreply.github.com>
2024-04-26 13:08:45 +0200

    Update testimonials.tsx

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -27,9 +27,8 @@ export default function Testimonials() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h2 className="h2 mb-4">Trusted by over 20,000 companies all over the world</h2>
-            <p className="text-xl text-gray-600" data-aos="zoom-y-out">Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis
-              blandit libero cursus mattis.</p>
+            <h2 className="h2 mb-4">Ondanks onze recente start hebben we nu al het vertrouwen gewonnen van bedrijven wereldwijd.</h2>
+            <p className="text-xl text-gray-600" data-aos="zoom-y-out">Met trots kunnen we zeggen dat ondanks onze recente oprichting, we reeds het vertrouwen hebben verdiend van bedrijven wereldwijd. Onze focus op professionaliteit, kwaliteit en klantenservice heeft ons snel doen onderscheiden.</p>
           </div>
 
           {/* Items */}
